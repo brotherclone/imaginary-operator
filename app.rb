e049b7fd4077e63aca8ec925d3f40be603645b8b
Gabriel Walsh <gawalsh@adobe.com>
2014-09-25 21:04:06 -0400

    fresh

--- app.rb
+++ app.rb
@@ -1,9 +1,7 @@
-require 'rubygems'
 require 'sinatra'
-require 'twilio-ruby'
 
-get '/index' do
+get '/' do
   Twilio::TwiML::Response.new do |r|
-    r.Say 'This is the Imaginary Foundation.'
+    r.Say 'Hello Monkey'
   end.text
 end
